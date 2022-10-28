75e7aa9eefe88053d4db7627b681d2a0d49ec587
root <RebaoneMabalane>
2022-10-27 19:59:05 -0700

    my sixth commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+char *string_tropper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
